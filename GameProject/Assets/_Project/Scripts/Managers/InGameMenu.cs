799687de6889d44c9637cab418975b3abd12eda5
Dogsh97 <hoony99@gmail.com>
2026-04-18 22:45:49 +0900

    소리 변경

--- GameProject/Assets/_Project/Scripts/Managers/InGameMenu.cs
+++ GameProject/Assets/_Project/Scripts/Managers/InGameMenu.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; // 씬 이동을 위해 필요
-
+using UnityEngine.Audio;
 public class InGameMenuController : MonoBehaviour
 {
     public GameObject pausePanel; // 메뉴 패널 오브젝트
     public static bool isPaused = false; // 일시정지 상태 여부
+    public AudioMixer MainAudioMixer;
 
     void Update()
     {
@@ -25,8 +26,9 @@ public class InGameMenuController : MonoBehaviour
         Time.timeScale = 1f;         // 시간 정상화
         isPaused = false;
 
-        // 모든 소리 다시 재생
-        AudioListener.pause = false;
+        // 다시 소리 키우기 (0dB가 기본)
+        MainAudioMixer.SetFloat("BGMVol", 0f);
+        MainAudioMixer.SetFloat("SFXVol", 0f);
 
         // 3D 마우스 커서 다시 숨기기
         Cursor.lockState = CursorLockMode.Locked;
@@ -39,8 +41,9 @@ public class InGameMenuController : MonoBehaviour
         Time.timeScale = 0f;         // 시간 정지
         isPaused = true;
 
-        // 모든 소리 일시정지
-        AudioListener.pause = true;
+        //소리 제거
+        MainAudioMixer.SetFloat("BGMVol", -80f); 
+        MainAudioMixer.SetFloat("SFXVol", -80f);
 
         // 마우스 커서 보이기 (버튼 클릭용)
         Cursor.lockState = CursorLockMode.None;
@@ -53,7 +56,9 @@ public class InGameMenuController : MonoBehaviour
         // 시간을 정상으로 돌린 후 로드 로직 실행
         Time.timeScale = 1f;
         isPaused = false;
-        AudioListener.pause = false; // 소리 해제
+        // 다시 소리 키우기 (0dB가 기본)
+        MainAudioMixer.SetFloat("BGMVol", 0f);
+        MainAudioMixer.SetFloat("SFXVol", 0f);
         Debug.Log("저장된 게임 데이터를 불러옵니다.");
         // 여기에 세이브 시스템 로드 코드를 추가하게 됩니다.
         // SceneManager.LoadScene("실제게임씬이름"); 
@@ -64,7 +69,9 @@ public class InGameMenuController : MonoBehaviour
     {
         Time.timeScale = 1f;
         isPaused = false;
-        AudioListener.pause = false; // 소리 해제
+        // 다시 소리 키우기 (0dB가 기본)
+        MainAudioMixer.SetFloat("BGMVol", 0f);
+        MainAudioMixer.SetFloat("SFXVol", 0f);
         // 빌드 설정(Build Settings)에 등록된 타이틀 씬 이름을 입력하세요.
         SceneManager.LoadScene("Title"); 
     }
